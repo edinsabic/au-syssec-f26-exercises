cee5379640a8010513ac1438fef83498097c0b1b
Edin Šabić <sabic.edin29@gmail.com>
2026-02-10 09:17:04 +0000

    Add exercise 1 & 2

--- 02_certificates_and_macs/rsa.py
+++ 02_certificates_and_macs/rsa.py
@@ -2,6 +2,21 @@ import sys
 import Crypto.Util.number
 from Crypto.Util.number import getPrime, GCD, inverse
 
+''' This exercise is the exact implementation of slide 44/60 of week 2's lecture
+"Crash course on Cryptography"
+It implements RSA key generation, encryption and decryption.
+
+Remember that textbook RSA is insecure and serves only for illustration, 
+in practice use a standardized version instead.
+
+RSA is defined over integers modulo N; 
+encrypting “bigger” numbers just collapses them to the same residue.
+
+Practical RSA encrypts arbitrary data by encoding it into an integer in [0,N−1]
+(padding/encoding schemes handle this).
+
+It is important to remember that in practice, RSA should not be used without proper padding
+schemes such as OAEP for encryption and PSS for signatures to ensure security against various attacks.'''
 
 def main(bits, message):
 
@@ -15,7 +30,10 @@ def main(bits, message):
         N = p * q
         phi = (p - 1) * (q - 1)
         e = 65537
-        # e needs to be invertible modulo phi(N)
+        ''' e needs to be co-prime to phi(N) and typically is chosen to be 65537, 
+        which is a common choice for the public exponent in RSA. It is a prime number 
+        and has certain properties that make it efficient for encryption and decryption operations.
+        e needs to be invertible modulo phi(N) '''
         if GCD(e, phi) > 1:
             continue
         d = inverse(e,phi)
@@ -29,6 +47,9 @@ def main(bits, message):
         break
 
     # encryption
+
+    # m € [0, N-1] (Z/NZ)
+    # compute c = m^e mod N
 
     m = message % N
     enc = pow(m, e, N)
