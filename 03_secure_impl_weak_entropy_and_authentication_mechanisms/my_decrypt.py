c514c0940e26f8f01de3a001a9512154b2e71e51
Edin Šabić <sabic.edin29@gmail.com>
2026-02-17 11:42:21 +0000

    Solve merge conflict

--- 03_secure_impl_weak_entropy_and_authentication_mechanisms/my_decrypt.py
+++ 03_secure_impl_weak_entropy_and_authentication_mechanisms/my_decrypt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-<<<<<<< HEAD
 import random
 import sys
 import time
@@ -12,27 +11,11 @@ def decrypt(input_file, output_file, time_range=86400):
     of the current time (or when ciphertext was created).
     time_range: search window in seconds (default: 24 hours)
     """
-=======
-from datetime import datetime
-import random
-import sys
-import time
-from tqdm import tqdm # install the tqdm package for a fancy progress bar
-from Crypto.Cipher import AES
-
-
-def decrypt(date, input_file, output_file):
-
-    date = datetime.strptime(date, '%Y-%m-%d')
-    t_start = int(date.timestamp())
-
->>>>>>> upstream/main
     with open(input_file, 'rb') as f_in:
         nonce = f_in.read(16)
         tag = f_in.read(16)
         ciphertext = f_in.read()
 
-<<<<<<< HEAD
     current_time = int(time.time())
     
     # Try times from (now - time_range) to (now + time_range)
@@ -65,27 +48,3 @@ if __name__ == '__main__':
     
     time_range = int(sys.argv[3]) if len(sys.argv) > 3 else 86400
     decrypt(sys.argv[1], sys.argv[2], time_range)
-=======
-    #  for t in range(t_start, t_start + (60 * 60 * 24)):  # <- use this instead of the following line if you don't have tqdm
-    for t in tqdm(range(t_start, t_start + (60 * 60 * 24))):
-        random.seed(t)
-        key = random.randbytes(16)
-        try:
-            aes = AES.new(key, AES.MODE_GCM, nonce=nonce)
-            plaintext = aes.decrypt_and_verify(ciphertext, tag)
-            break
-        except:
-            continue
-    else:
-        print('decryption failed')
-
-    with open(output_file, 'wb') as f_out:
-        f_out.write(plaintext)
-
-
-if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print(f'usage: {sys.argv[0]} <date> <src-file> <dst-file>', file=sys.stderr)
-        exit(1)
-    decrypt(sys.argv[1], sys.argv[2], sys.argv[3])
->>>>>>> upstream/main
